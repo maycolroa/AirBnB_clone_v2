49bd6f054b35306fce3c41e0bdc3004469ba12b8
maycolroa <lehallo@hotmail.com>
2022-05-24 18:30:48 -0500

    0x03. AirBnB clone - Deploy static 0.02 24/05/22

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Write a Fabric script (based on the file 1-pack_web_static.py)"""
-from fabric.api import run, put, env, local
-import os
+
+from fabric.api import local, settings, abort, run, cd, env, put, get
+from fabric.decorators import task, hosts, with_settings
+from os import path
 from datetime import datetime
 
 
@@ -9,35 +11,35 @@ env.hosts = ["34.139.187.216", "34.204.18.63"]
 env.user = "ubuntu"
 
 def do_pack():
-    """fuction"""
-    try:
-        local("mkdir -p versions")
-        d_t = datetime.now().strftime("%Y%m%d%H%M%S")
-        compr_file = "versions/web_static_{}.tgz".format(d_t)
-        local("tar -cvzf {} web_static".format(compr_file))
-        return compr_file
-    except:
+    """function"""
+    local("mkdir -p versions")
+    name = "web_static_{}".format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    ruta = "versions/{}".format(name)
+    local("tar czfv versions/{}.tgz web_static".format(name))
+
+    if path.isfile(ruta):
+        return ruta
+    else:
         return None
 
 
 def do_deploy(archive_path):
-    """funtion"""
-    name_dir = archive_path[9:-4]
-    if os.path.exists(archive_path):
-        put(archive_path, '/tmp/')
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(name_dir))
-        run("sudo tar -xzf /tmp/{}.tgz -C \
-        /data/web_static/releases/{}/".format(name_dir, name_dir))
-        run("sudo rm /tmp/{}.tgz".format(name_dir))
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".
-            format(name_dir, name_dir))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(name_dir))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current".format(name_dir))
+    """function"""
+    if path.isfile(archive_path):
+        name_file = archive_path[9:]
+        new_path = "/data/web_static/releases/"
+        path_server_file = "/tmp/{}".format(name_file)
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(new_path))
+        run("sudo tar -xzf {} -C {}/".format(path_server_file, new_path))
+        run("sudo rm {}".format(path_server_file))
+        run("sudo rm -rf {}".format("/data/web_static/current"))
+        run(
+            "sudo ln -s {} /data/web_static/current".format(
+                "/data/web_static/releases/web_static"
+            )
+        )
         print("New version deployed!")
         return True
-    else:
-        return False
+
+    return False
