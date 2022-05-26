875be6fdc3fa8376ddee62f46f898b6e41106a2e
maycolroa <lehallo@hotmail.com>
2022-05-25 21:05:44 -0500

    0x04. AirBnB clone - Web framework 0.01 25/05/22

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """Write a script that starts a Flask web application:"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def main():
-    """message"""
+@app.route('/', strict_slashes=False)
+def index():
+    """message Hello HBNB!"""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """message"""
+@app.route('/hbnb', strict_slashes=False)
+def index2():
+    """message HBNB"""
     return "HBNB"
 
 
